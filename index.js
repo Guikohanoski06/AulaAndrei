0e567a1fb6898b693236558042365554c1cd0adc
Guilherme Kohanoski <kohanoskiguilherme@gmail.com>
2024-03-05 11:31:03 -0300

    aula andrei

---
+++ index.js
@@ -0,0 +1,12 @@
+//comando para exibir mensagem
+console.log("O GREMIO VAI SER CANPEÃo E o inter não !");
+
+const num1 = 20;
+const num2 = 654;
+
+let soma = num1 + num2
+console.log(`O valor da soima é ${soma}`);
+
+
+
+
